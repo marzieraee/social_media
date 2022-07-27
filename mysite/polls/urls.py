9c3e26db9a1c96d6bfdc5b22a318966ec23f937c
marzieraee <marzyra1997@gmail.com>
2022-07-27 11:53:46 +0430

    login

---
+++ mysite/polls/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+#     path('posts/', views.PostList.as_view(), name='postlistapi'),
+#     path('postsingle/<int:pk>', views.SinglePost.as_view(), name='postsingle'),
+#     path('postcreate/', views.PostComment.as_view(), name='postcomment'),
+#     # path('commentretrieve/<int:pk>', views.CommentRetriveApi.as_view(), name='commentretriveapi'),
+#     path('register/', views.SignUp.as_view(), name='register'),
+#     path('profile/<int:pk>', views.Profile.as_view(), name='profile'),
+#     path('changepassword/<int:pk>', views.ChangePass.as_view(), name='changepass'),
+#     path('createcomment/<int:pk>', views.CreateComment.as_view(), name='createcomment'),
+#     path('like/<int:pk>', views.Like.as_view(), name='like'),
+#     path('logout/', views.Logout.as_view()),
+    
+    
+    
+    
+]
